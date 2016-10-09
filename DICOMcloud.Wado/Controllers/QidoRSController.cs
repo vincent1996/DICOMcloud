8ef253f191338c0166cca3425d32ab0ec504e1b4
zaid al-safadi <zsafadi11@gmail.com>
2016-10-09 00:01:49 -0400

    Add build event to copy DLL to ../dist folder

--- DICOMcloud.Wado/Controllers/QidoRSController.cs
+++ DICOMcloud.Wado/Controllers/QidoRSController.cs
@@ -8,6 +8,11 @@ using DICOMcloud.Wado.Models;
 namespace DICOMcloud.Wado.Controllers
 {
     //[Authorize]
+    /// <summary>
+    /// Query based on ID for DICOM Objects (QIDO) enables you to search for studies, series and instances by patient ID, 
+    /// and receive their unique identifiers for further usage (i.e., to retrieve their rendered representations). More detail can be found in PS3.18 6.7.
+    /// https://dicomweb.hcintegrations.ca/services/query/
+    /// </summary>
     public class QidoRSController : ApiController
     {
         protected QidoRsService QidoService {get; set;}
@@ -17,6 +22,11 @@ namespace DICOMcloud.Wado.Controllers
             QidoService = qidoService ;
         }
 
+        /// <summary>
+        /// Look up studies (i.e., for a particular patient)
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
         [Route("qidors/studies")]
         [HttpGet]
         public HttpResponseMessage SearchForStudies
@@ -28,6 +38,11 @@ namespace DICOMcloud.Wado.Controllers
             return QidoService.SearchForStudies ( request ) ;
         }
 
+        /// <summary>
+        /// Look up a series
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
         [Route("qidors/studies/{StudyInstanceUID}/series")]
         [Route("qidors/series")]
         [HttpGet]
@@ -40,7 +55,11 @@ namespace DICOMcloud.Wado.Controllers
             return QidoService.SearchForSeries ( request ) ;
         }
 
-
+        /// <summary>
+        /// Look up instances
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
         [Route("qidors/studies/{StudyInstanceUID}/series/{SeriesInstanceUID}/instances")]
         [Route("qidors/studies/{StudyInstanceUID}/instances")]
         [Route("qidors/instances")]
